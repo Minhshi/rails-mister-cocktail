47e59c5408ba1e30daad044d82fe4007f362cfda
Minh Pham <mpham@naver.com>
2018-11-16 06:55:06 +0100

    Finish Backend of Mister Cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating cocktails...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+data_serialized = open(url).read
+data = JSON.parse(data_serialized)
+
+#puts "#{data['drinks'][0]['strIngredient1']}"
+
+data['drinks'].each do |d|
+  Ingredient.create(name: "#{d['strIngredient1']}")
+end
+
+puts 'Finished!'
+
+# Ingredient.create(name: "lemon")
